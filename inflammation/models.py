79b09be9969805dbb871780e6b76f774b5419c71
Tom J. Wilson <t.j.wilson@exeter.ac.uk>
2021-12-10 09:58:49 +0000

    Updated inflammation code with stuff, and tests for that stuff.

--- inflammation/models.py
+++ inflammation/models.py
@@ -61,14 +61,60 @@ def patient_normalise(data):
         raise ValueError('inflammation array should be 2-dimensional')
     if np.any(data < 0):
         raise ValueError('inflammation values should be non-negative')
-    max = np.nanmax(data, axis=1)
+    _max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / _max[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
 
 
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
 
-# TODO(lesson-design) Add Patient class
-# TODO(lesson-design) Implement data persistence
-# TODO(lesson-design) Add Doctor class
+    def __str__(self):
+        return self.value
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+
+        self.observations = []
+        if observations is not None:
+            self.observations = observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(value, day)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+
+class Doctor(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient):
+        new_patient = Patient(patient)
+
+        self.patients.append(new_patient)
+        return new_patient
